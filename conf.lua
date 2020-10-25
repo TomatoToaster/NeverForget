7dd38126175593fcd31f25612e9936ea28e1b084
Amal Nazeem <amalnazeem@gmail.com>
2020-10-25 02:02:40 -0400

    Create conf.lua

---
+++ conf.lua
@@ -0,0 +1,13 @@
+--[[
+  This is a file that I can use to play with some settings within a Love2D
+  game. I just have to drop this in the main directory with the main.lua.
+  These are common settings I found useful
+
+  Check out more at: https://love2d.org/wiki/Config_Files
+]]
+function love.conf(t)
+  -- Settings I toggle on while developing:
+  t.console = true
+
+
+end
